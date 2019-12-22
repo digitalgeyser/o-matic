8c7a5e79b31ed1f1ded878383f5aacf73ed694c7
Timotej <timotej@digitalgeyser.com>
2019-12-22 13:48:34 -0500

    The buttons are now working.

--- the_box/the_box.ino
+++ the_box/the_box.ino
@@ -8,6 +8,7 @@
 #include <RTClib.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+#include <Button.h>
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
@@ -17,9 +18,20 @@ LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars
 #define IN_SENSOR_TYPE DHT11
 #define OUT_SENSOR_TYPE DHT11
 
+#define BUTTON_COUNT 5
+Button button_next(22);
+Button button_before(23);
+Button button_inc(24);
+Button button_dec(25);
+Button button_sel(26);
+
+Button buttons[5] = { button_next, button_before, button_inc, button_dec, button_sel };
+
 RTC_DS3231 clock;
-DHT insideSensor(TEMPERATURE_HUMIDITY_SENSOR_IN_PIN, IN_SENSOR_TYPE);
-DHT outsideSensor(TEMPERATURE_HUMIDITY_SENSOR_OUT_PIN, OUT_SENSOR_TYPE);
+//DHT insideSensor(TEMPERATURE_HUMIDITY_SENSOR_IN_PIN, IN_SENSOR_TYPE);
+//DHT outsideSensor(TEMPERATURE_HUMIDITY_SENSOR_OUT_PIN, OUT_SENSOR_TYPE);
+
+unsigned long lastLcdRefresh = 0;
 
 void updateLcd(LiquidCrystal_I2C lcd,
                const char *line0,
@@ -38,7 +50,9 @@ void updateLcd(LiquidCrystal_I2C lcd,
 }
 
 void setup() {
-  Serial.begin(9600);
+  int i;
+
+  Serial.begin(115200);
   Serial.println(F("Paint!"));
   lcd.init();  //initialize the lcd
   lcd.backlight();  //open the backlight 
@@ -50,20 +64,34 @@ void setup() {
   delay(3000);
 
   clock.begin();
+
+  for ( i = 0; i<BUTTON_COUNT; i++ ) {
+    buttons[i].begin();
+  }
 }
 
-void loop() {
-  Serial.println(F("Loop."));
+
+void lcdTick() {
   DateTime now = clock.now();
-  Serial.println(F("Got clock."));
-  Serial.println(now.second(), DEC);
-char buf2[] = "MM/DD/YYYY  hh:mm:ss";
-   Serial.println(now.toString(buf2));
-    updateLcd(lcd,
+  char buf2[] = "MM/DD/YYYY  hh:mm:ss";
+  updateLcd(lcd,
             "     The Box!       ",
             "   Digital Geyser   ",
             "                    ",
             now.toString(buf2));
-  Serial.println(F("Updated LCD."));
-  delay(1000);
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+
+  if ( button_next.pressed() ) Serial.println("next");
+  if ( button_before.pressed() ) Serial.println("before");
+  if ( button_inc.pressed() ) Serial.println("inc");
+  if ( button_dec.pressed() ) Serial.println("dec");
+  if ( button_sel.pressed() ) Serial.println("sel");
+
+  if ( currentTime - lastLcdRefresh > 1000 ) {
+    lcdTick();
+    lastLcdRefresh = currentTime;
+  }
 }
